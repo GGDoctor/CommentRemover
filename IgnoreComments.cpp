d46e652a0fa61e7604563b8042c3aceb2c7f4479
Hbjoerger <97217742+Hbjoerger@users.noreply.github.com>
2024-01-31 12:01:59 -0800

    Inputting testing files
    Added code to input the providing testing files.

--- IgnoreComments.cpp
+++ IgnoreComments.cpp
@@ -7,29 +7,41 @@
 // Zach Gassner
 
 
-// I will actually add in the code on my work computer tomorrow but
-// I'm bored right now so I just started this but I need to rework it since
-// he wants switch statements
-
-
 #include <iostream>
+#include <fstream> //Including fstream and string allow us to read in the input file
+#include <string>
+using namespace std;
 
 //Create the suggested State
-//Anything is anything that is not a comment
 enum State {
     ANYTHING, 
     SLASH,
-    SINGLE_LINE,
-    MULTIPLE_LINE
+    SINGLE_LINE, // A comment pertaining '//'
+    MULTIPLE_LINE // A comment pertaining '/* bla */
 };
 
 
-int main() {
 
+int main(int argc, char *argv[]) {
+    string filename = argv[1];
+    ifstream inputFile(filename.c_str());
 
-    //Write a cin for the input file
+    if (!inputFile) {
+        std::cerr << "Error. Unable to open the file :( \n"; // Error out if we are unable to open up the file
+        return 1;
+    }
 
+    string line;
+    while (getline(inputFile, line)) {
+        cout << line << endl; // Outputting the file for us to read 
+    }
 
-
-
+    inputFile.close(); // Close the file when we are done working.
+    return 0;
 }
+
+
+//I downloaded the file and put them into the project
+//then did a g++ -o IgnoreComments IgnoreComments.cpp
+//then did ./IgnoreComments programming_assignment_1-test_file_2.c
+//this worked.
