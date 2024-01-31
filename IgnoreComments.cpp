ea5fb5cd15c7352123db3ad2647d924d91ec4766
Hbjoerger <97217742+Hbjoerger@users.noreply.github.com>
2024-01-31 09:33:15 -0800

    Create IgnoreComments.cpp

---
+++ IgnoreComments.cpp
@@ -0,0 +1,35 @@
+// Ignore comments
+// The objective of this project is to take the first step torwards building an interpreter, ignoring comments
+// This project is developed by the following students:
+// Haley Joerger
+// Adam Lyday
+// Jacob Franco
+// Zach Gassner
+
+
+// I will actually add in the code on my work computer tomorrow but
+// I'm bored right now so I just started this but I need to rework it since
+// he wants switch statements
+
+
+#include <iostream>
+
+//Create the suggested State
+//Anything is anything that is not a comment
+enum State {
+    ANYTHING, 
+    SLASH,
+    SINGLE_LINE,
+    MULTIPLE_LINE
+};
+
+
+int main() {
+
+
+    //Write a cin for the input file
+
+
+
+
+}
